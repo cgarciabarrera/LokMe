b715745495119559e89b055b5868ad500b38cd08
carlos <carlos@carlos-12SSD.(none)>
2013-10-06 16:19:33 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LokMe::Application.config.secret_key_base = '2e6a6b524339828ec5ed787a2c8e21f523ffecb897752b2ff665d504ff03bb2531b48d6d723daaabf28f04cd65104dad52935304013e0aafffc64a65b3e06617'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LokMe::Application.config.session_store :cookie_store, key: '_LokMe_session'
