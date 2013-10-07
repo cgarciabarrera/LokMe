6dc4d4c7de0d058e0449bbd362f61ff19ef7a5d0
carlos <carlos@carlos-12SSD.(none)>
2013-10-07 22:10:54 +0200

    Error seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,19 @@
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
 puts 'ROLES'
+#YAML.load(ENV['ROLES']).each do |role|
+#  Role.find_or_create_by_name(role)
+#  puts 'role: ' << role
+#end
+
 YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name(role)
+  puts role
+  Role.where(name: role).first_or_create
+  #Role.find_or_create_by_name({ :name => role }, :without_protection => true)
   puts 'role: ' << role
 end
+
+
 puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
