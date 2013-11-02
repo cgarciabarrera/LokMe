0379c15015b6696d61abbfbaf870f15d808d6583
Carlos García de la Barrera <carlos.garcia@lextrend.net>
2013-11-02 14:58:26 +0100

    Mas maps

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,7 +13,7 @@ class HomeController < ApplicationController
       #@devices=Device.all
       #@devices= Device.where('user_id = ?', current_user.id)
 
-      @devices = Device.find_by_sql("select * from devices where user_id = " + current_user.id.to_s + " union  select * from devices where id in (select device_id from shareds where user_id = " + current_user.id.to_s + ")")
+      @devices = Device.find_by_sql("select * from devices where user_id = " + current_user.id.to_s + " union  select * from devices where id in (select device_id from shareds where user_shared = " + current_user.id.to_s + ")")
 
       @json       = @devices.to_gmaps4rails
     end
