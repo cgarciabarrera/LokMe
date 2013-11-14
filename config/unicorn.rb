4f8772d5b81942704c8321ea362b7b798e6c635b
Carlos García de la Barrera s76 <carlos.garcia@lextrend.net>
2013-11-14 17:21:27 +0100

    Mas correcciones

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/env ruby
 
-listen "/tmp/.lokme_com_unicorn.sock"
+listen "/tmp/.freeditorial_com_unicorn.sock"
 
 worker_processes 4
 
@@ -11,14 +11,14 @@ if ENV['RAILS_ENV'] == 'staging'
   Dir = "lokme.lextrendlabs.com/"
 else
   timeOut = 4000
-  Dir = "www.xxx.com/"
+  Dir = "www.freeditorial.com/"
 end
 
 working_directory APP_PATH + Dir + "current/"
 timeout timeOut
 pid APP_PATH + Dir + "current/tmp/pids/unicorn.pid"
-stderr_path APP_PATH + Dir + "current/log/lokme.stderr.log"
-stdout_path APP_PATH + Dir + "current/log/lokme.stdout.log"
+stderr_path APP_PATH + Dir + "current/log/freeditorial.stderr.log"
+stdout_path APP_PATH + Dir + "current/log/freeditorial.stdout.log"
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) and
