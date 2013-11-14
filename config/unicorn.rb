b9ccbdc1ce330598ac1766499de71db0aaee22e5
Carlos García de la Barrera s76 <carlos.garcia@lextrend.net>
2013-11-14 18:14:23 +0100

    Environment corregido

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -18,8 +18,8 @@ end
 working_directory APP_PATH + Dir + "current/"
 timeout timeOut
 pid APP_PATH + Dir + "current/tmp/pids/unicorn.pid"
-stderr_path APP_PATH + Dir + "current/log/freeditorial.stderr.log"
-stdout_path APP_PATH + Dir + "current/log/freeditorial.stdout.log"
+stderr_path APP_PATH + Dir + "current/log/lokme.stderr.log"
+stdout_path APP_PATH + Dir + "current/log/lokme.stdout.log"
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) and
